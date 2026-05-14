653bca0ae9ee43878dde21d07212beaf146bdeb1
wallacezhang <164129345@qq.com>
2026-05-14 20:02:18 +0800

    update code

--- get_Started/src/main.cpp
+++ get_Started/src/main.cpp
@@ -6,12 +6,12 @@
 
 // 1.定义事件（普通struct, 随意定义字段）
 namespace Event {
-    struct SensorData {
+    struct MotorTemperature {
         int sensorId;
         float temperature;
     };
 
-    struct SystemError {
+    struct RobotError {
         std::string message;
         int errorCode;
     };
@@ -22,21 +22,21 @@ class DataProcessor {
     public:
         explicit DataProcessor(std::shared_ptr<dexode::EventBus> bus) : _listener(bus) {
             // 3. 注册监听 -- lambda方式
-            _listener.listen([this](const Event::SensorData& data) {
-                onSensorData(data);
+            _listener.listen([this](const Event::MotorTemperature& data) {
+                onMotorTemperature(data);
             });
 
             // 也可以直接绑定成员函数
-            _listener.listen<Event::SystemError>(std::bind(&DataProcessor::onError, this, std::placeholders::_1));
+            _listener.listen<Event::RobotError>(std::bind(&DataProcessor::onError, this, std::placeholders::_1));
         }
 
     private:
-        void onSensorData(const Event::SensorData& data) {
-            std::cout << "Received Sensor Data: ID=" << data.sensorId << ", Temp=" << data.temperature << std::endl;
+        void onMotorTemperature(const Event::MotorTemperature& data) {
+            std::cout << "Received Motor Temperature: ID=" << data.sensorId << ", Temp=" << data.temperature << std::endl;
         }
 
-        void onError(const Event::SystemError& error) {
-            std::cout << "Received System Error: " << error.message << " (Code: " << error.errorCode << ")" << std::endl;
+        void onError(const Event::RobotError& error) {
+            std::cout << "Received Robot Error: " << error.message << " (Code: " << error.errorCode << ")" << std::endl;
         }
 
         dexode::EventBus::Listener _listener; // RAII, 析构时自动注销
@@ -49,10 +49,10 @@ int main() {
     DataProcessor processor(bus);
 
     // 4. 投递事件（放入队列，不立即触发）
-    bus->postpone(Event::SensorData{1, 36.5f});
-    bus->postpone(Event::SensorData{2, 22.3f});
+    bus->postpone(Event::MotorTemperature{1, 36.5f});
+    bus->postpone(Event::MotorTemperature{2, 22.3f});
 
-    bus->postpone(Event::SystemError{"Overheating detected", 408});
+    bus->postpone(Event::RobotError{"Overheating detected", 408});
 
     // 5. 处理所有排队的事件（同步执行所有回调）
     bus->process();
